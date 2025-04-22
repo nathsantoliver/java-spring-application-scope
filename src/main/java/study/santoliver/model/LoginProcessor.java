3b926137fb39387701ed553756e73fd752999643
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-21 22:16:01 -0300

    incrementing the login count for every login request

--- src/main/java/study/santoliver/model/LoginProcessor.java
+++ src/main/java/study/santoliver/model/LoginProcessor.java
@@ -3,21 +3,27 @@ package study.santoliver.model;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 import study.santoliver.services.LoggedUserManagementService;
+import study.santoliver.services.LoginCountService;
 
 @Component
 @RequestScope
 public class LoginProcessor {
 
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
 
     private String username;
     private String password;
 
-    public LoginProcessor(LoggedUserManagementService loggedUserManagementService) {
+    public LoginProcessor(LoggedUserManagementService loggedUserManagementService,
+                          LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     public boolean login() {
+        loginCountService.increment();
+
         String username = this.getUsername();
         String password = this.getPassword();
 
