be827c5fa3c0bc7a4295740ac6358f8fde98e17a
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-21 21:44:52 -0300

    plublishing the core of the project

---
+++ src/main/java/study/santoliver/SpringApplicationScopeApplication.java
@@ -0,0 +1,13 @@
+package study.santoliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplicationScopeApplication.class, args);
+	}
+
+}
