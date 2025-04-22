3e0ac1eac31c8eb16443b39f827c7e1881aafd3f
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-21 22:19:28 -0300

    sending the count value from controller to be displayed on the main page

--- src/main/java/study/santoliver/controllers/MainController.java
+++ src/main/java/study/santoliver/controllers/MainController.java
@@ -5,32 +5,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import study.santoliver.services.LoggedUserManagementService;
+import study.santoliver.services.LoginCountService;
 
 @Controller
 public class MainController {
 
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
 
-    public MainController(LoggedUserManagementService loggedUserManagementService) {
+    public MainController(LoggedUserManagementService loggedUserManagementService,
+                          LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     @GetMapping("/main")
-    public String home(
-            @RequestParam(required = false) String logout,
-            Model model
-    ) {
+    public String home(@RequestParam(required = false) String logout, Model model) {
         if (logout != null) {
             loggedUserManagementService.setUsername(null);
         }
 
         String username = loggedUserManagementService.getUsername();
+        int count = loginCountService.getCount();
 
         if (username == null) {
             return "redirect:/";
         }
 
         model.addAttribute("username" , username);
+        model.addAttribute("loginCount" , count);
 
         return "main.html";
     }
